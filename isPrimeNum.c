6d1bbbdbb8e2b74b4463c63b8675e9f3f5f8defb
SKMAAAAAX <skmaxm@gmail.com>
2025-03-03 20:51:01 +0800

    优化显示效果

--- isPrimeNum.c
+++ isPrimeNum.c
@@ -25,6 +25,7 @@ enum TypeOfNum isPrimeNum(long long int num)
     return prime;
 }
 
+//主程序
 int main()
 {
     system("cls");
@@ -39,7 +40,7 @@ int main()
         printf("1.判断单一数字是否为质数\n");
         printf("2.判断范围内的数字是否为质数\n");
         printf("输入选项(1或2)：");
-        while (getchar() != '\n') // 清空输入缓冲区
+        while (getchar() != '\n') 
             ;
     }
     if (option == 1)
@@ -52,7 +53,7 @@ int main()
         while (scanf("%lld", &num) != 1 || num <= 1)
         {
             system("cls");
-            printf("输入有误，请重新输入一个有效的自然数：");
+            printf("非法输入，请重新输入一个有效的自然数：");
             while (getchar() != '\n') // 清空输入缓冲区
                 ;
         }
@@ -86,24 +87,25 @@ int main()
         while (scanf("%lld", &start) != 1 || start <= 1)
         {
             system("cls");
-            printf("输入有误，请重新输入一个有效的自然数：");
+            printf("非法输入，请重新输入：");
             while (getchar() != '\n') // 清空输入缓冲区
                 ;
         }
-
-        // 输入验证：确保输入为有效的大于起始数字的整数
-        printf("输入结束数字：");
-        while (scanf("%lld", &end) != 1 || end <= start)
-        {
             system("cls");
-            printf("输入有误，请重新输入一个有效的自然数：");
-            while (getchar() != '\n') // 清空输入缓冲区
-                ;
+            printf("起始数字为：%lld\n", start);
+            // 输入验证：确保输入为有效的大于起始数字的整数
+            printf("输入结束数字：");
+            while (scanf("%lld", &end) != 1 || end <= start)
+            {
+                system("cls");
+                printf("起始数字为：%lld\n", start);
+                printf("非法输入，请重新输入：");
+                while (getchar() != '\n') // 清空输入缓冲区
+                    ;
         }
-
         // 显示结果
         system("cls");
-        printf("范围内的质数有：\n");
+        printf("%lld--%lld内的质数有：\n",start,end);
         int count = 0;
         for (long long int i = start; i <= end; i++)
         {
@@ -111,11 +113,11 @@ int main()
             {
                 printf("%lld\t", i);
                 count++;
-                if (count % 5 == 0)
+                if (count % 7 == 0)
                     printf("\n");
             }
         }
-        if (count % 5 != 0)
+        if (count % 7 != 0)
             printf("\n");
         printf("范围内的质数数量为：%d\n", count);
         return 0;
